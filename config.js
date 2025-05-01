393f17590216cb3c3b8b78c2cadd66d6f11dd345
Chamijd <chamindumax28@gmail.com>
2025-05-01 09:32:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=SYYzRCrT#vb_GycozhQb6KL3zVf-fgI9cHZ1TjW6W4p1cvBkE-5c",
+SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=2UxywCLI#unLvXI9vL2NQkbPUukig_65TTRsqtA5V-9S6Ey_GZds",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -55,7 +55,7 @@ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // make true for automatic stickers 
